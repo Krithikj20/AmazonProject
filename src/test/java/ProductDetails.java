a7f1d4a80d5bb96823f6acc9b34b27730f27069e
krithikj20 <krithik@geekyants.com>
2024-10-09 13:54:42 +0530

    Modified productDetails page and added included methods in testNG.xml file

--- src/test/java/ProductDetails.java
+++ src/test/java/ProductDetails.java
@@ -4,9 +4,16 @@ import org.testng.annotations.Test;
 
 public class ProductDetails extends BaseTest{
     @Test
-    public void productDetails() {
+    public void productSearch() {
+        ProductDetailsPageObjects details = new ProductDetailsPageObjects(driver);
+        details.setSearch();
+    }
+    @Test
+    public void productVerifyDetails() {
         ProductDetailsPageObjects details = new ProductDetailsPageObjects(driver);
         details.setSearch();
         details.verifyDetails();
     }
-}
+
+    }
+
